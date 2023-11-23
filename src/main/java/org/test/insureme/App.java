08a99a6bbc25b9aafc0ffa24eb6624347a90c24d
vijayalakshmipushpendra <146848558+vijayalakshmipushpendra@users.noreply.github.com>
2023-11-23 12:11:07 +0530

    Update App.java

--- src/main/java/org/test/insureme/App.java
+++ src/main/java/org/test/insureme/App.java
@@ -35,7 +35,7 @@ public class App
     // open url
     System.out.println("Driver opening up the url in browser");	
     WebDriver driver = new ChromeDriver(chromeOptions);
-    driver.get("http://13.232.51.121:8081/contact.html");	
+    driver.get("http://13.232.165.53:8081/contact.html");	
     
     //invole implicit waits to load the page
     driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
